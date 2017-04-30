46b35a6e0a3bc68a61eb9161c60e3fa9bbdc49ca
gmadmin <gmadmin@Futureland.com>
2017-04-30 19:37:17 +0100

    skus and sizes

--- AzureResourceGroupGM/Azurewebapp.ps1
+++ AzureResourceGroupGM/Azurewebapp.ps1
@@ -1,4 +1,4 @@
-#Login-AzureRmAccount 
+Login-AzureRmAccount 
 Get-AzureRmSubscription | Select-AzureRmSubscription
 Get-AzureRmSubscription | Set-AzureRmContext 
 
@@ -6,6 +6,10 @@ $location = "westeurope"
 
 Get-AzureRmVMImagePublisher -Location $location
 Get-AzureRmVMImageOffer -Location $location
-  
+#get list of sizes for vms
+Get-AzureRmVMSize $location | Select-Object name 
+
+#get list of operating systems
+Get-AzureRmVMImageSku  -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Location $location
 
 #Get-AzureRmVMImage  -Location $location -PublisherName "microsoft" 
