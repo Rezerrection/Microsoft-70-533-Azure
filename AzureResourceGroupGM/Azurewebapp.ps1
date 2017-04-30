dda36d3906b20c8a515f28992f5f3fe45cf0cd82
gmadmin <gmadmin@Futureland.com>
2017-04-30 19:10:12 +0100

    ROLES

--- AzureResourceGroupGM/Azurewebapp.ps1
+++ AzureResourceGroupGM/Azurewebapp.ps1
@@ -6,8 +6,6 @@ $location = "westeurope"
 
 Get-AzureRmVMImagePublisher -Location $location
 Get-AzureRmVMImageOffer -Location $location
+  
 
-
-
-
-Get-AzureRmVMImage  -Location $location -PublisherName "microsoft" -o
+#Get-AzureRmVMImage  -Location $location -PublisherName "microsoft" 
