93ef695a696a23d99908c7cef8edb91493135d10
gmadmin <gmadmin@Futureland.com>
2017-04-30 19:43:35 +0100

    final stuff

--- AzureResourceGroupGM/Azurewebapp.ps1
+++ AzureResourceGroupGM/Azurewebapp.ps1
@@ -3,15 +3,19 @@ Get-AzureRmSubscription | Select-AzureRmSubscription
 Get-AzureRmSubscription | Set-AzureRmContext 
 
 $location = "westeurope"
-
+$publisher = "MicrosoftWindowsServer"
+$offer = "WindowsServer"
+#list of image publishers, main one for vms is MicrosoftWindowsServer it seems
 Get-AzureRmVMImagePublisher -Location $location
-Get-AzureRmVMImageOffer -Location $location
+
+#then you have to get the offer, which appears to be WindowsServer
+Get-AzureRmVMImageOffer -Location $location -PublisherName $publisher
 
 #get list of sizes for vms
 Get-AzureRmVMSize $location | Select-Object name 
 
 
 #get list of operating systems
-Get-AzureRmVMImageSku  -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Location $location
+Get-AzureRmVMImageSku  -PublisherName $publisher -Offer $offer -Location $location
 
 #Get-AzureRmVMImage  -Location $location -PublisherName "microsoft" 
