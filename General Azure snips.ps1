5c0e8be1abd6a75a21acf28b0a5be5bfbbaa88da
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-05-15 18:03:56 +0100

    rbac

--- General Azure snips.ps1
+++ General Azure snips.ps1
@@ -1,1 +1,13 @@
 (Get-AzureRmResourceProvider  -ProviderNamespace microsoft.compute).locations | sort -Unique
+
+Get-AzureRmProviderOperation -OperationSearchString "microsoft.compute/*"
+
+Get-AzureRmProviderOperation -OperationSearchString "microsoft.compute/virtualMachines*/action" | ft Operation, operationname
+
+Get-AzureRmRoleDefinition
+
+
+
+
+ git config --global user.email "gabriel@cloudconfusion.co.uk"
+  git config --global user.name "gabrielmccoll"
