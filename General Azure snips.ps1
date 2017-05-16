8f1fb182a8ac6dd761294e9594a7863c2c581de9
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-05-16 08:17:05 +0100

    vm disk stuff

--- General Azure snips.ps1
+++ General Azure snips.ps1
@@ -8,3 +8,18 @@ Get-AzureRmRoleDefinition
 
 
 
+Get-AzureRmRoleAssignment 
+#can do it fo resource group or for individual -expandprincipalgroups to see the groups they're in and have permissions hat way. 
+
+Get-AzureRmAuthorizationChangeLog -StartTime ([datetime]::Now - [timespan]::FromDays((7)
+
+Set-AzureRmVMAccessExtension  #name is just name of the action
+
+#if have to manually install the service agent then you have to
+#update the azure fabric of the machine to let it know
+$vm = Get-AzureRmVm -ResourceGroupName -Name
+$vm.OSProfile.WindowsConfiguration.ProvisionVMAgent = $true
+update-azurermvm -ResourceGroupName -VM $vm 
+
+get-azurermvm | Set-AzureRmVMOSDisk
+Get-AzureRmVM | Set-AzureRmVMOSDisk
