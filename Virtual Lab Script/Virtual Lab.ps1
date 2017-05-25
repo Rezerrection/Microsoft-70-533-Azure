9060abe333d5f54376b16585d3700a52baa4c31e
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-05-25 15:25:31 +0100

    add steps for the lab

--- Virtual Lab Script/Virtual Lab.ps1
+++ Virtual Lab Script/Virtual Lab.ps1
@@ -1,7 +1,9 @@
 <# 
 1. Create virtual netwrk with 172.1.1.1/16 
-2. Create subnect 172.1.2.1 
+2. Create subnet 172.1.2.1 
 3. Create Subnet 172.1.3.1 
-
-
+4. Create NSGs for each
+5. Create 3 Storage accounts - VMs , App, Diagnostics
+6. Create 2 Availability Sets VMs and APPs 
+7. Create 2 VMs into availability set , one in each subnet
 #>
