452e1ab4522a19312023dbf5805a80b5b0e9e25c
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-05-25 16:57:37 +0100

    set out script for creating test lab specs more

--- Virtual Lab Script/Virtual Lab.ps1
+++ Virtual Lab Script/Virtual Lab.ps1
@@ -1,9 +1,10 @@
 <# 
-1. Create virtual netwrk with 172.1.1.1/16 
-2. Create subnet 172.1.2.1 
-3. Create Subnet 172.1.3.1 
+1. Create virtual netwrk with 172.16.0.0/16 
+2. Create subnet 172.16.5.0 /24
+3. Create Subnet 172.16.10.0 /24
 4. Create NSGs for each
 5. Create 3 Storage accounts - VMs , App, Diagnostics
 6. Create 2 Availability Sets VMs and APPs 
-7. Create 2 VMs into availability set , one in each subnet
+7. Create 2 basic srv 2016 VMs into availability set , one in each subnet, one with managed disk, one with blod storage
+
 #>
