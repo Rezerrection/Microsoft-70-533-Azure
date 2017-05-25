55f4a63c6e8466e75a34ff2236bbbcd1d4cd4f79
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-05-25 12:50:15 +0100

    added objectives to VMs

--- 70-533 VM/70533VM.ps1
+++ 70-533 VM/70533VM.ps1
@@ -1,3 +1,28 @@
+<# 
+Things to do for the virtual machines
+- Create new virtual machines, Portal and Powershell - maybe CLI
+- Take an existing VM from on prem into Azure
+- Config with Powershell DSC and VM Agent.   
+- enable remote debugging ? 
+- Add a new disk to existing VM.   
+- Configure disk caching
+- plan storage capacity
+-  hconfigure operating system disk redundancy
+- configure shared storage using Azure File service
+- configure geo-replication
+- encrypt disks
+- implement ARM VMs with Standard and Premium Storage
+- Monitoring metrics on VMs - Portal and PShell
+- Configure Alerts ad rules for cpu, storage, ram 
+- Configure multiple ARM VMs in an availability set for redundancy
+- configure each application tier into separate availability sets
+- combine the Load Balancer with availability sets
+- Scale up and scale down VM sizes
+- deploy ARM VM Scale Sets (VMSS)
+- configure ARM VMSS auto-scale
+/#>
+
+
 Login-AzureRmAccount 
 Get-AzureRmSubscription | Select-AzureRmSubscription
 Get-AzureRmSubscription | Set-AzureRmContext 
--- General Azure snips.ps1
+++ General Azure snips.ps1
@@ -1,3 +1,32 @@
+<# 
+Things to do for the virtual machines
+- Create new virtual machines, Portal and Powershell - maybe CLI
+- Take an existing VM from on prem into Azure
+- Config with Powershell DSC and VM Agent.   
+- enable remote debugging ? 
+- Add a new disk to existing VM.   
+- Configure disk caching
+- plan storage capacity
+- configure operating system disk redundancy
+- configure shared storage using Azure File service
+- configure geo-replication
+- encrypt disks
+- implement ARM VMs with Standard and Premium Storage
+- Monitoring metrics on VMs - Portal and PShell
+- Configure Alerts ad rules for cpu, storage, ram 
+- Configure multiple ARM VMs in an availability set for redundancy
+- configure each application tier into separate availability sets
+- combine the Load Balancer with availability sets
+- Scale up and scale down VM sizes
+- deploy ARM VM Scale Sets (VMSS)
+- configure ARM VMSS auto-scale
+/#>
+
+
+
+
+
+
 (Get-AzureRmResourceProvider  -ProviderNamespace microsoft.compute).locations | sort -Unique
 
 Get-AzureRmProviderOperation -OperationSearchString "microsoft.compute/*"
