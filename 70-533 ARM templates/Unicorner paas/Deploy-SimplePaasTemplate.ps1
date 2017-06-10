b8f7c85c8b4a1ba101c131f58fc4c6b0983ac2f2
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2017-06-10 13:37:55 +0100

    add unicorner

---
+++ 70-533 ARM templates/Unicorner paas/Deploy-SimplePaasTemplate.ps1
@@ -0,0 +1,25 @@
+﻿### Define variables
+{
+$resourcegrouplocation = 'west europe'
+$resourceGroupName = 'pluralsight-arm-unicornerpaas-templategm'
+$resourceDeploymentName = 'pluralsight-arm-unicornerpaas-template-deploymentgm'
+$templatePath = 'C:\Users\breau\Documents\Visual Studio 2017\Projects'
+$templateFile = 'unicornerPaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourcegrouplocation `
+    -Verbose -Force
+
+
+### Deploy Resources
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
--- 70-533 Webapps/70533webapps.ps1
+++ 70-533 Webapps/70533webapps.ps1
@@ -12,7 +12,7 @@ $resourcegroupname = "TestGMAzureAppPlanPS"
 $appserviceplanname = "TestGMAzureAPPPlanPS1"
 $appserviceplanname2 = "TestGmAzureAppPlanPS2"
 $webappname = "TestGMAzureWebAppPS1"
-New-AzureRmResourceGroup -Name $resourcegroupname -Location $location 
+New-AzureRAmResourceGroup -Name $resourcegroupname -Location $location 
 New-AzureRmAppServicePlan -name $appserviceplanname -Location $location -ResourceGroupName $resourcegroupname -Tier Free -NumberofWorkers 1 -WorkerSize Small
 New-AzureRmAppServicePlan -name $appserviceplanname2 -Location $location -ResourceGroupName $resourcegroupname -Tier Free -NumberofWorkers 1 -WorkerSize Small
 
@@ -26,38 +26,38 @@ Set-AzureRmWebApp -Name $webappname -ResourceGroupName $resourcegroupname -AppSe
 # Set-AzureRmWebAppSlot -Name <webapp name> -Slot <slot name> -ResourceGroupName <resource group name> -AppServicePlan <new app service plan>
 
 
-$gitrepo="<Replace with your GitHub repo URL>"
-$webappname="mywebapp$(Get-Random)"
-$location="West Europe"
+$gitrepo = "<Replace with your GitHub repo URL>"
+$webappname = "mywebapp$(Get-Random)"
+$location = "West Europe"
 
 # Create a resource group.
 New-AzureRmResourceGroup -Name myResourceGroup -Location $location
 
 # Create an App Service plan in Free tier.
 New-AzureRmAppServicePlan -Name $webappname -Location $location `
--ResourceGroupName myResourceGroup -Tier Free
+    -ResourceGroupName myResourceGroup -Tier Free
 
 # Create a web app.
 New-AzureRmWebApp -Name $webappname -Location $location `
--AppServicePlan $webappname -ResourceGroupName myResourceGroup
+    -AppServicePlan $webappname -ResourceGroupName myResourceGroup
 
 # Upgrade App Service plan to Standard tier (minimum required by deployment slots)
 Set-AzureRmAppServicePlan -Name $webappname -ResourceGroupName myResourceGroup `
--Tier Standard
+    -Tier Standard
 
 #Create a deployment slot with the name "staging".
 New-AzureRmWebAppSlot -Name $webappname -ResourceGroupName myResourceGroup `
--Slot staging
+    -Slot staging
 
 # Configure GitHub deployment to the staging slot from your GitHub repo and deploy once.
 $PropertiesObject = @{
     repoUrl = "$gitrepo";
-    branch = "master";
+    branch  = "master";
 }
 Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName myResourceGroup `
--ResourceType Microsoft.Web/sites/slots/sourcecontrols `
--ResourceName $webappname/staging/web -ApiVersion 2015-08-01 -Force
+    -ResourceType Microsoft.Web/sites/slots/sourcecontrols `
+    -ResourceName $webappname/staging/web -ApiVersion 2015-08-01 -Force
 
 # Swap the verified/warmed up staging slot into production.
 Swap-AzureRmWebAppSlot -Name $webappname -ResourceGroupName $resourcegroupname `
--SourceSlotName "test" -DestinationSlotName production
+    -SourceSlotName "test" -DestinationSlotName production
